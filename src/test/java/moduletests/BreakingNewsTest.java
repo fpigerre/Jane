13ba12a29e8d73989d5e34412da1c94496a6a1f3
psgs <fpigerre@gmail.com>
2014-06-08 17:47:50 +1000

    Disable BreakingNewsTest is Twitter is disabled
    Disable BreakingNewsTest from executing if the Twitter accessToken and
    accessTokenSecret aren't valid.

--- src/test/java/moduletests/BreakingNewsTest.java
+++ src/test/java/moduletests/BreakingNewsTest.java
@@ -9,7 +9,7 @@ import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.Date;
 import java.util.List;
 
@@ -17,28 +17,70 @@ import static org.mockito.Mockito.validateMockitoUsage;
 
 public class BreakingNewsTest {
 
+    // Whether Twitter keys are loaded correctly
+    static boolean validAccessToken = false;
+    static boolean validAccessTokenSecret = false;
+
     @Test
     public void readBreakingTweet() {
-        Twitter twitter = TwitterFactory.getSingleton();
+        if (validAccessToken && validAccessTokenSecret) {
+            Twitter twitter = TwitterFactory.getSingleton();
+            try {
+                List<Status> statuses = twitter.getUserTimeline("BreakingNews");
+                Date date = new Date(System.currentTimeMillis() - 305000);
+                for (Status status : statuses) {
+                    if (status.getCreatedAt().after(date)) {
+                        assert status.getId() != 0 && !status.getText().equals("");
+                        ReadTweet.execute(status);
+                    }
+                }
+            } catch (TwitterException ex) {
+                System.out.println("An error occurred while trying to fetch a tweet!");
+                if (ex.getErrorCode() == 34) {
+                    System.out.println("The tweet you requested wasn't found!");
+                    try {
+                        AudioUtils.talk("I tried to access a tweet, but it wasn't found.");
+                    } catch (FileNotFoundException exception) {
+                        System.out.println("A result couldn't be spoken!!");
+                    }
+                }
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * Attempts to find Twitter keys contained in the twitter4j.properties file and load them
+     */
+    public static void refreshTwitterKeys() {
         try {
-            List<Status> statuses = twitter.getUserTimeline("BreakingNews");
-            Date date = new Date(System.currentTimeMillis() - 305000);
-            for (Status status : statuses) {
-                if (status.getCreatedAt().after(date)) {
-                    assert status.getId() != 0 && !status.getText().equals("");
-                    ReadTweet.execute(status);
+            if (new File("twitter4j.properties").exists()) {
+                BufferedReader bufferedReader = new BufferedReader(new FileReader("twitter4j.properties"));
+                StringBuilder stringBuilder = new StringBuilder();
+                String line = bufferedReader.readLine();
+
+                while (line != null) {
+                    if (line.contains("oauth.accessToken=")) {
+                        String rawToken = line.replace("oauth.accessToken=", "");
+                        if (!rawToken.equals("") && !rawToken.equals(" ") && rawToken != null) {
+                            validAccessToken = true;
+                        }
+                    }
+                    if (line.contains("oauth.accessTokenSecret=")) {
+                        String rawTokenSecret = line.replace("oauth.accessTokenSecret=", "");
+                        if (!rawTokenSecret.equals(" ") && rawTokenSecret.equals("") && rawTokenSecret != null) {
+                            validAccessTokenSecret = true;
+                        }
+                    }
+                    stringBuilder.append(line);
+                    stringBuilder.append(System.lineSeparator());
+                    line = bufferedReader.readLine();
                 }
+            } else {
+                validAccessToken = false;
+                validAccessTokenSecret = false;
             }
-        } catch (TwitterException ex) {
-            System.out.println("An error occurred while trying to fetch a tweet!");
-            if (ex.getErrorCode() == 34) {
-                System.out.println("The tweet you requested wasn't found!");
-                try {
-                    AudioUtils.talk("I tried to access a tweet, but it wasn't found.");
-                } catch (FileNotFoundException exception) {
-                    System.out.println("A result couldn't be spoken!!");
-                }
-            }
+        } catch (IOException ex) {
             ex.printStackTrace();
         }
     }
