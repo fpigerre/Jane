b572c5c927da9d04c0a51a543dd807664d9338c1
psgs <fpigerre@gmail.com>
2014-06-08 17:54:35 +1000

    Add Linux check for TrayIconTest
    Use file paths to check if TrayIconTest is running on Linux. If so, stop
    the test from being executed.

--- src/test/java/TrayIconTest.java
+++ src/test/java/TrayIconTest.java
@@ -2,13 +2,18 @@ import io.github.psgs.jane.interfaces.SystemTrayIcon;
 import org.junit.After;
 import org.junit.Test;
 
+import java.io.File;
+
 import static org.mockito.Mockito.validateMockitoUsage;
 
 public class TrayIconTest {
 
     @Test
     public void initiateTrayIcon() {
-        SystemTrayIcon trayIcon = new SystemTrayIcon();
+        // Check whether we're running on Linux
+        if (!new File("/tmp/Jane.lock").exists()) {
+            SystemTrayIcon trayIcon = new SystemTrayIcon();
+        }
     }
 
     @After
