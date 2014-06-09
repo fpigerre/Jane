ea8d7e122191e87518505c0835faa0b0315bf83f
psgs <fpigerre@gmail.com>
2014-06-09 19:50:53 +1000

    Trigger refresh before BreakingNewsTest
    Trigger a Twitter key refresh before the BreakingNewsTest is executed.
    Set validAccessToken and validAccessTokenSecret fields to false after
    the test executes.

--- src/test/java/moduletests/BreakingNewsTest.java
+++ src/test/java/moduletests/BreakingNewsTest.java
@@ -3,6 +3,7 @@ package moduletests;
 import io.github.psgs.jane.modules.ReadTweet;
 import io.github.psgs.jane.utilities.AudioUtils;
 import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import twitter4j.Status;
 import twitter4j.Twitter;
@@ -21,37 +22,10 @@ public class BreakingNewsTest {
     static boolean validAccessToken = false;
     static boolean validAccessTokenSecret = false;
 
-    @Test
-    public void readBreakingTweet() {
-        if (validAccessToken && validAccessTokenSecret) {
-            Twitter twitter = TwitterFactory.getSingleton();
-            try {
-                List<Status> statuses = twitter.getUserTimeline("BreakingNews");
-                Date date = new Date(System.currentTimeMillis() - 305000);
-                for (Status status : statuses) {
-                    if (status.getCreatedAt().after(date)) {
-                        assert status.getId() != 0 && !status.getText().equals("");
-                        ReadTweet.execute(status);
-                    }
-                }
-            } catch (TwitterException ex) {
-                System.out.println("An error occurred while trying to fetch a tweet!");
-                if (ex.getErrorCode() == 34) {
-                    System.out.println("The tweet you requested wasn't found!");
-                    try {
-                        AudioUtils.talk("I tried to access a tweet, but it wasn't found.");
-                    } catch (FileNotFoundException exception) {
-                        System.out.println("A result couldn't be spoken!!");
-                    }
-                }
-                ex.printStackTrace();
-            }
-        }
-    }
-
     /**
      * Attempts to find Twitter keys contained in the twitter4j.properties file and load them
      */
+    @Before
     public static void refreshTwitterKeys() {
         try {
             if (new File("twitter4j.properties").exists()) {
@@ -85,8 +59,38 @@ public class BreakingNewsTest {
         }
     }
 
+    @Test
+    public void readBreakingTweet() {
+        if (validAccessToken && validAccessTokenSecret) {
+            Twitter twitter = TwitterFactory.getSingleton();
+            try {
+                List<Status> statuses = twitter.getUserTimeline("BreakingNews");
+                Date date = new Date(System.currentTimeMillis() - 305000);
+                for (Status status : statuses) {
+                    if (status.getCreatedAt().after(date)) {
+                        assert status.getId() != 0 && !status.getText().equals("");
+                        ReadTweet.execute(status);
+                    }
+                }
+            } catch (TwitterException ex) {
+                System.out.println("An error occurred while trying to fetch a tweet!");
+                if (ex.getErrorCode() == 34) {
+                    System.out.println("The tweet you requested wasn't found!");
+                    try {
+                        AudioUtils.talk("I tried to access a tweet, but it wasn't found.");
+                    } catch (FileNotFoundException exception) {
+                        System.out.println("A result couldn't be spoken!!");
+                    }
+                }
+                ex.printStackTrace();
+            }
+        }
+    }
+
     @After
     public void validate() {
+        validAccessToken = false;
+        validAccessTokenSecret = false;
         validateMockitoUsage();
     }
 }
