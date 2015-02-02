07e29b06b5caacdc5d8ab957f326a8dc7a7af519
psgs <fpigerre@gmail.com>
2015-02-02 20:34:27 +1000

    Elaborate module selection

--- src/main/java/io/github/psgs/jane/utilities/AmbientListener.java
+++ src/main/java/io/github/psgs/jane/utilities/AmbientListener.java
@@ -2,7 +2,7 @@ package io.github.psgs.jane.utilities;
 
 import edu.cmu.sphinx.api.Configuration;
 import edu.cmu.sphinx.api.LiveSpeechRecognizer;
-import io.github.psgs.jane.modules.Time;
+import io.github.psgs.jane.modules.*;
 
 import java.io.IOException;
 
@@ -12,7 +12,6 @@ public class AmbientListener {
      * Starts an Ambient Listening process that listens for speech and attempts to convert it to text.
      */
     public static void ambientListening() throws IOException {
-        System.out.println("Ambient listening started...");
         Configuration configuration = new Configuration();
 
         // Set path to acoustic model.
@@ -29,9 +28,35 @@ public class AmbientListener {
         LiveSpeechRecognizer recognizer = new LiveSpeechRecognizer(configuration);
         recognizer.startRecognition(true);
         while (true) {
-            String utterance = recognizer.getResult().getHypothesis();
-            if (utterance.equals("time")) {
+            String utterance = recognizer.getResult().getHypothesis().replace("jane", "");
+            if (utterance.equals("time") || utterance.equals("date")) {
                 Time.execute();
+            }
+            if (utterance.contains("github")) {
+                if (utterance.equals("github")) {
+                    GitHubJobs.execute();
+                } else {
+                    String[] command = utterance.split(" ");
+                    if (command[0].equals("github")) {
+                        GitHubJobs.execute(command[1]);
+                    }
+                }
+            }
+            if (utterance.equals("panic panic panic")) {
+                Panic.execute();
+            }
+            if (utterance.contains("tweets")) {
+                String[] command = utterance.split(" ");
+                if (command.length > 1) {
+                    if (command[0].equals("tweets")) {
+                        ReadTweet.execute(command[1]);
+                    }
+                } else {
+                    System.out.println("An argument wasn't detected for the Twitter command!");
+                }
+            }
+            if (utterance.contains("google")) {
+                GoogleSearch.execute(utterance.replace("google", ""));
             }
             if (utterance.equals("one zero one") || utterance.equals("one oh one")) {
                 break;
